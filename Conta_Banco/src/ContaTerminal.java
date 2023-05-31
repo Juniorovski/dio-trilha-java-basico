824683149ba5ef206ddaba373884dcf97afc18a7
Júnior_Melo <juniornine19@gmail.com>
2023-05-30 22:52:33 -0300

    Atividade pronta

--- Conta_Banco/src/ContaTerminal.java
+++ Conta_Banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try {
+            System.out.println("<======== BEM VINDOS AO BANCO RM DO CURSO JAVA DA ***DIO*** ========>");
+
+    Scanner scan = new Scanner(System.in);
+
+        System.out.println("digite seu nome:");
+        String nome = scan.nextLine();
+        System.out.println("Digite o numero da conta:");
+        int conta = scan.nextInt();
+        System.out.println("Digite o numero da sua agencia:");
+        int agencia = scan.nextInt();
+        System.out.println("Deposite seu saldo:");
+        double saldo = scan.nextDouble();
+        
+        System.out.printf("Olá %s obrigado por abrir uma conta na nosso banco, sua agencia %d, conta %d e seu saldo é: %.2f" , nome,agencia,conta,saldo );
+    
+        } catch (Exception e) {
+           System.out.println(e);
+        }
     }
 }
